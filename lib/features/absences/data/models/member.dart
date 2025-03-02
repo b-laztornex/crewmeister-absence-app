ac080e176109031a71ed2dc2fd8c3f87baeb68ab
Marco Casanova <m.casanova.dev@gmail.com>
2025-03-02 19:30:07 +0100

    add userId to member model

--- lib/features/absences/data/models/member.dart
+++ lib/features/absences/data/models/member.dart
@@ -2,15 +2,26 @@ import 'package:equatable/equatable.dart';
 
 class Member extends Equatable {
   final int id;
+  final int userId;
   final String name;
   final String image;
 
-  Member({required this.id, required this.name, required this.image});
+  const Member({
+    required this.id,
+    required this.userId,
+    required this.name,
+    required this.image,
+  });
 
   factory Member.fromJson(Map<String, dynamic> json) {
-    return Member(id: json['id'], name: json['name'], image: json['image']);
+    return Member(
+      id: json['id'],
+      userId: json['userId'],
+      name: json['name'],
+      image: json['image'],
+    );
   }
 
   @override
-  List<Object?> get props => [id, name, image];
+  List<Object?> get props => [id, userId, name, image];
 }
