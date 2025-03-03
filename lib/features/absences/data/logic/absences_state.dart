76fb76f6ce129d602d2ca1a1d5ba73b781645113
Marco Casanova <m.casanova.dev@gmail.com>
2025-03-03 16:55:28 +0100

    add filter type and date to keep track of fliters

--- lib/features/absences/data/logic/absences_state.dart
+++ lib/features/absences/data/logic/absences_state.dart
@@ -1,6 +1,10 @@
 import 'package:equatable/equatable.dart';
 import '../../data/models/absence.dart';
 
+/*
+holds variables to keep track of filters (absence type, start and end dates) and the current page for pagination.
+*/
+
 abstract class AbsencesState extends Equatable {
   const AbsencesState();
   @override
@@ -14,16 +18,30 @@ class AbsencesLoaded extends AbsencesState {
   final int currentPage;
   final int totalPages;
   final int totalAbsences;
+  final String? typeFilter;
+  final DateTime? startDate;
+  final DateTime? endDate;
 
   const AbsencesLoaded({
     required this.absences,
     required this.currentPage,
     required this.totalPages,
     required this.totalAbsences,
+    this.typeFilter,
+    this.startDate,
+    this.endDate,
   });
 
   @override
-  List<Object?> get props => [absences, currentPage, totalPages, totalAbsences];
+  List<Object?> get props => [
+    absences,
+    currentPage,
+    totalPages,
+    totalAbsences,
+    typeFilter,
+    startDate,
+    endDate,
+  ];
 }
 
 class AbsencesEmpty extends AbsencesState {}
