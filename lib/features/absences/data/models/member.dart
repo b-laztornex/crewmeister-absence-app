0f80657576a1f487a767fbad04d8f8187c266253
Marco Casanova <m.casanova.dev@gmail.com>
2025-03-05 18:01:14 +0100

    add crewid to member model  and match the attributes names from the api response

--- lib/features/absences/data/models/member.dart
+++ lib/features/absences/data/models/member.dart
@@ -3,25 +3,28 @@ import 'package:equatable/equatable.dart';
 class Member extends Equatable {
   final int id;
   final int userId;
+  final int crewId;
   final String name;
   final String image;
 
   const Member({
     required this.id,
     required this.userId,
+    required this.crewId,
     required this.name,
     required this.image,
   });
 
   factory Member.fromJson(Map<String, dynamic> json) {
     return Member(
-      id: json['id'] ?? 0,
-      userId: json['userId'] ?? 0,
+      id: json['id'],
+      userId: json['userid'],
+      crewId: json['crewid'],
       name: json['name'] ?? 'Unknown',
       image: json['image'] ?? '',
     );
   }
 
   @override
-  List<Object?> get props => [id, userId, name, image];
+  List<Object?> get props => [id, userId, crewId, name, image];
 }
