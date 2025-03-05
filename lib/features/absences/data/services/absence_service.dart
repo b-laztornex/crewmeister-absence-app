fb966c34456dce15aca4f0c26220fca207682a21
Marco Casanova <m.casanova.dev@gmail.com>
2025-03-05 11:27:54 +0100

    update the absence service to consume data from api endpoints

--- lib/features/absences/data/services/absence_service.dart
+++ lib/features/absences/data/services/absence_service.dart
@@ -1,21 +1,41 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'package:cm_absence_manager/config/api_config.dart';
+import 'package:http/http.dart' as http;
 import '../models/absence.dart';
 import '../models/member.dart';
 
 class AbsenceService {
   Future<List<Absence>> fetchAbsences() async {
-    await Future.delayed(Duration(seconds: 1)); // simulate network delay
-    final jsonString = await rootBundle.loadString('assets/absences.json');
-    final Map<String, dynamic> data = jsonDecode(jsonString);
-    final List<dynamic> payload = data['payload'];
-    return payload.map((item) => Absence.fromJson(item)).toList();
+    final url = '${ApiConfig.baseUrl}/absences';
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List<dynamic> payload = data['payload'];
+      return payload.map((item) => Absence.fromJson(item)).toList();
+    } else {
+      throw Exception("Failed to load absences");
+    }
   }
 
   Future<List<Member>> fetchMembers() async {
-    final jsonString = await rootBundle.loadString('assets/members.json');
-    final Map<String, dynamic> data = jsonDecode(jsonString);
-    final List<dynamic> payload = data['payload'];
-    return payload.map((item) => Member.fromJson(item)).toList();
+    final url = '${ApiConfig.baseUrl}/members';
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List<dynamic> payload = data['payload'];
+      return payload.map((item) => Member.fromJson(item)).toList();
+    } else {
+      throw Exception("Failed to load members");
+    }
   }
 }
